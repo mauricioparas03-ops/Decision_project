950293a036352c2baae9727cbfedaa98abc04f0b
matteobrol <brolpasino.m@gmail.com>
2026-05-16 19:54:08 +0200

    Training modified deleted max r1 and r2

--- PART_B/ADP_Training.py
+++ PART_B/ADP_Training.py
@@ -76,19 +76,11 @@ def solve_bellman_equation_milp(state, next_t_weights):
     m.v = Var(domain=Binary)
     
         # Overrule stato corrente (vincola l'azione here-and-now)
-    if state['low_override_r1'] == 1 and state['T1'] < data['temp_OK_threshold']:
-        m.p1.fix(data['heating_max_power'])
-    if state['T1'] > data['temp_max_comfort_threshold']:
-        m.p1.fix(0)
-
-    if state['low_override_r2'] == 1 and state['T2'] < data['temp_OK_threshold']:
-        m.p2.fix(data['heating_max_power'])
-    if state['T2'] > data['temp_max_comfort_threshold']:
-        m.p2.fix(0)
-
-    if state['H'] > data['humidity_threshold'] or \
-    (state['vent_counter'] > 0 and state['vent_counter'] < data['vent_min_up_time']):
-        m.v.fix(1)
+    if state['T1'] > data['temp_max_comfort_threshold']: m.p1.fix(0)
+    elif state['T1'] < data['temp_min_comfort_threshold'] or state['low_override_r1'] == 1: m.p1.fix(data['heating_max_power'])
+    if state['T2'] > data['temp_max_comfort_threshold']: m.p2.fix(0)
+    elif state['T2'] < data['temp_min_comfort_threshold'] or state['low_override_r2'] == 1: m.p2.fix(data['heating_max_power'])
+    if state['H'] > data['humidity_threshold'] or state['vent_counter'] in [1, 2]: m.v.fix(1)
 
     scen_data = []
     for _ in range(K_SCENARIOS):
@@ -100,7 +92,6 @@ def solve_bellman_equation_milp(state, next_t_weights):
     m.Scen = RangeSet(0, K_SCENARIOS - 1)
     m.T1_next = Var(m.Scen); m.T2_next = Var(m.Scen)
     m.H_next = Var(m.Scen)
-    m.s = Var(domain=Binary)
     m.ov1_next = Var(m.Scen, domain=Binary)
     m.ov2_next = Var(m.Scen, domain=Binary)
     m.vc_next = Var(domain=NonNegativeReals)
@@ -144,15 +135,7 @@ def solve_bellman_equation_milp(state, next_t_weights):
         m.H_next[k] == state['H'] - data['humidity_vent_coeff']*m.v + 
                        data['humidity_occupancy_coeff']*(state['Occ1']+state['Occ2']))
 
-    # Temperature Cutoff and heater deactivation
-    m.cutoff_r1 = Constraint(m.Scen, rule=lambda m, k: m.T1_next[k] >= data['temp_max_comfort_threshold'] - M*(1 - m.y_high_r1[k]))
-    m.cutoff_r2 = Constraint(m.Scen, rule=lambda m, k: m.T2_next[k] >= data['temp_max_comfort_threshold'] - M*(1 - m.y_high_r2[k]))
-    m.cutin_r1 = Constraint(m.Scen, rule=lambda m, k: m.T1_next[k] <= data['temp_max_comfort_threshold'] + M * m.y_high_r1[k])
-    m.cutin_r2 = Constraint(m.Scen, rule=lambda m, k: m.T2_next[k] <= data['temp_max_comfort_threshold'] + M * m.y_high_r2[k])
-
-    # Overrule forcing heater to 0
-    m.overrule_r1 = Constraint(m.Scen, rule=lambda m, k: m.p1 <= data['heating_max_power'] * (1 - m.y_high_r1[k]))
-    m.overrule_r2 = Constraint(m.Scen, rule=lambda m, k: m.p2 <= data['heating_max_power'] * (1 - m.y_high_r2[k]))
+    
     # =========================================================
     # 3. LOGICA OVERRULE STANZA 1
     # =========================================================
@@ -183,22 +166,6 @@ def solve_bellman_equation_milp(state, next_t_weights):
     m.c_keep_override_ON_until_ok_r2 = Constraint(m.Scen, rule=lambda m, k: m.ov2_next[k] >= u_prev_r2 - m.y_ok_r2[k])
     m.c_force_override_OFF_if_ok_r2 = Constraint(m.Scen, rule=lambda m, k: m.ov2_next[k] <= 1 - m.y_ok_r2[k])
 
-    # Overrule controller forcing heater to maximum
-    m.max_r1 = Constraint(m.Scen, rule=lambda m, k: m.p1 >= data['heating_max_power'] * m.ov1_next[k])
-    m.max_r2 = Constraint(m.Scen, rule=lambda m, k: m.p2 >= data['heating_max_power'] * m.ov2_next[k])
-
-    m.c_startup_lb = Constraint(rule=lambda m : m.s >= m.v - v_prev)
-
-    # ── Constraint 2: s_t <= v_t  (startup solo se ventilazione è attiva)
-    m.c_startup_ub1 = Constraint(rule=lambda m : m.s <= m.v)
-
-    # ── Constraint 3: s_t <= 1 - v_{t-1}  (startup solo se prima era spenta)
-    if state['current_time'] > 0:
-        m.c_startup_ub2 = Constraint(rule=lambda m : m.s <= 1 - v_prev)
-    
-    remaining_uptime = data["vent_min_up_time"] - state['vent_counter']
-    if state['vent_counter'] > 0 and remaining_uptime > 0:
-        m.v.fix(1)   # già presente nel tuo codice con [1,2]
         
     #humidity triggered ventilation
     m.humidity_trigger = Constraint(m.Scen, rule=lambda m, k: m.H_next[k] <= data['humidity_threshold'] + M*m.v)
