6aa9b4bde3714532a8e83e52cbd319c88dedd013
DavideCominelli <dvd.cominelli@gmail.com>
2026-05-16 23:46:40 +0200

    added header on environment

--- PART_B/Environment.py
+++ PART_B/Environment.py
@@ -14,10 +14,10 @@ from v2_Checks import check_and_sanitize_action
 
 data_dir = Path(__file__).resolve().parent / "Data"
 
-occ1 = pd.read_csv(data_dir / "OccupancyRoom1.csv").values.flatten()
-occ2 = pd.read_csv(data_dir / "OccupancyRoom2.csv").values.flatten()
+occ1 = pd.read_csv(data_dir / "OccupancyRoom1.csv", header=None).values.flatten()
+occ2 = pd.read_csv(data_dir / "OccupancyRoom2.csv", header=None).values.flatten()
 
-df_prices = pd.read_csv(data_dir / "v2_PriceData.csv") # skip header, as we will access the columns by index
+df_prices = pd.read_csv(data_dir / "v2_PriceData.csv", header=None) # skip header, as we will access the columns by index
 
 # extract the first column (previous price) as a separate array, if needed for the dynamics or the policy.
 daily_previous_prices = df_prices.iloc[:, 0].values 
